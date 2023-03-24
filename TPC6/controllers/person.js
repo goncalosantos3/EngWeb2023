eebb5d4522bb878c99ef60c7f5250a02f582922d
goncalosantos3 <a95354@alunos.uminho.pt>
2023-03-24 21:07:52 +0000

    Inicio TPC6

---
+++ TPC6/controllers/person.js
@@ -0,0 +1,52 @@
+var Person = require('../models/person')
+
+// Person list
+module.exports.list = () => {
+    return Person.find()
+        .then(resposta => {
+            return resposta
+        })
+        .catch(erro => {
+            return erro
+        })
+}
+
+module.exports.getPerson = id => {
+    return Person.findOne({_id: id})
+        .then(resposta => {
+            return resposta
+        })
+        .catch(erro => {
+            return erro
+        })
+}
+
+module.exports.addPerson = p => {
+    return Person.create(p)
+        .then(resposta => {
+            return resposta
+        })
+        .catch(erro => {
+            return erro
+        })
+}
+
+module.exports.updatePerson = (p, id) => {
+    return Person.updateOne({_id: id}, p)
+        .then(resposta => {
+            return resposta
+        })
+        .catch(erro => {
+            return erro
+        })
+}
+
+module.exports.deletePerson = id => {
+    return Person.deleteOne({_id: id})
+        .then(resposta => {
+            return resposta
+        })
+        .catch(erro => {
+            return erro
+        })
+}
---
+++ TPC6/routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var Person = require('../controllers/person')
+
+/* --------------------------------------- GET ---------------------------------------------- */
+/* GET home page. */
+router.get('/people', function(req, res) {
+  Person.list()
+    .then(dados => res.status(200).json(dados))
+    .catch(erro => res.status(520).json({erro: erro, message: "Não consegui obter a lista de pessoas!"}))
+});
+
+router.get('/people/:id', (req,res) => {
+  Person.getPerson(req.params.id)
+    .then(dados => res.status(200).json(dados))
+    .catch(erro => res.status(521).json({erro: erro, message: "Não consegui obter o registo da pessoa!"}))
+})
+
+/* --------------------------------------- POST ---------------------------------------------- */
+
+router.post('/people', (req, res) => {
+  Person.addPerson(req.body)
+    .then(dados => res.status(201).json(dados))
+    .catch(erro => res.status(522).json({erro: erro, message: "Não consegui inserir o registo da pessoa!"}))
+})
+
+/* --------------------------------------- PUT ----------------------------------------------- */
+
+router.put('/people/:id', (req, res) => {
+  Person.updatePerson(req.body, req.params.id)
+    .then(dados => res.status(200).json(dados))
+    .catch(erro => res.status(523).json({erro: erro, message: "Não consegui atualizar o registo da pessoa!"}))
+})
+
+/* --------------------------------------- DELETE -------------------------------------------- */
+
+router.delete('/people/:id', (req, res) => {
+  Person.deleteExame(req.params.id)
+    .then(dados => res.status(200).json(dados))
+    .catch(erro => res.status(524).json({erro: erro, message: "Não consegui eliminar o registo da pessoa!"}))
+})
+
+module.exports = router;
